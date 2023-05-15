5b49dda7eecf91a128b0bb524bd1ed2ec82aedaf
Eddy98 <eduardodgraziano@gmail.com>
2023-05-15 08:11:57 -0400

    feat: client side unit tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { expect, test } from '@jest/globals';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+  test('Renders Eduardo Graziano', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ message: 'Eduardo Graziano' }),
+      })
+    );
+
+    const { findByText } = render(<App />);
+    expect(await findByText('Eduardo Graziano')).toBeVisible();
+  });
+
+  test('Renders unexpected error', async () => {
+    fetch.mockImplementationOnce(() => Promise.reject('API is down'));
+
+    const { findByText } = render(<App />);
+    expect(await findByText('Unexpected error')).toBeVisible();
+  });
 });
